b53759e2e14a656fe106a576eb45f320c07bf81f
Karolina Machnik <machnik.karolina98@gmail.com>
2024-05-16 11:34:13 +0200

    update in ContactForm component

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -13,7 +13,7 @@ export const ContactForm = ({
     <>
       <form onSubmit={handleSubmit}>
         <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Name"  required />
-        <input type="number" value={phone} onChange={(e) => setPhone(e.target.value)} pattern="[0-9]{9}" placeholder="Phone Number (ANNNNNNNN)" required/>
+        <input type="tel" value={phone} onChange={(e) => setPhone(e.target.value)} pattern="([0-9]+(-[0-9]+)+)" placeholder="Phone Number - format: XXX-XXX-XXX" required/>
         <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="E-mail adress" required />
         <button>Add contact</button>
       </form>
